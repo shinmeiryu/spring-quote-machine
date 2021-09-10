10b6450ff3c5b26063b9c7e55a0325ad680e367b
Jeffrey Wong <j.wong@greenvalley.nl>
2021-09-10 15:51:37 +0200

    Initial spring init

---
+++ src/main/java/com/jeff/springquotemachine/SpringQuoteMachineApplication.java
@@ -0,0 +1,13 @@
+package com.jeff.springquotemachine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringQuoteMachineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringQuoteMachineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jeff/springquotemachine/SpringQuoteMachineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeff.springquotemachine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringQuoteMachineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
